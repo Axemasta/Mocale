9c051ee6de49d643af26c8fb28ec39bc72cf5943
Axemasta <33064621+Axemasta@users.noreply.github.com>
2022-11-13 16:48:22 +0000

    Solution creation
    Created the mocale project and samples to test as I go

---
+++ samples/Mocale.Samples/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Mocale.Samples;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
---
+++ src/Mocale/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Mocale
+{
+    // All the code in this file is included in all platforms.
+    public class Class1
+    {
+    }
+}
