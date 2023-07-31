39cce078b6655dce3575887d6dde701c0ef510b3
Axemasta <33064621+Axemasta@users.noreply.github.com>
2023-07-31 13:29:57 +0100

    Added Source Generators Project
    Start working on adding the source gen to automatically create our localization keys. Switching to windows for this one because the tooling just appears more adept to handle this dev

--- src/Mocale.Providers.AWS.S3/S3BucketProvider.cs
+++ src/Mocale.Providers.AWS.S3/S3BucketProvider.cs
@@ -1,11 +1,50 @@
 using System.Globalization;
 using Mocale.Abstractions;
+using Mocale.Models;
 namespace Mocale.Providers.AWS.S3;
 
 internal sealed class S3BucketProvider : IExternalLocalizationProvider
 {
-    public Task<IExternalLocalizationResult> GetValuesForCultureAsync(CultureInfo cultureInfo)
+#pragma warning disable CS1998
+    public async Task<IExternalLocalizationResult> GetValuesForCultureAsync(CultureInfo cultureInfo)
+#pragma warning restore CS1998
     {
-        throw new NotImplementedException();
+        if (cultureInfo.ToString() == "en-GB")
+        {
+            return new ExternalLocalizationResult()
+            {
+                Success = true,
+                Localizations = new Dictionary<string, string>()
+                {
+                    { "CurrentLocaleName", "S3English" },
+                    { "LocalizationCurrentProviderIs", "S3The current localization provider is:" },
+                    { "LocalizationProviderName", "S3Json" },
+                    { "MocaleDescription", "S3Localization framework for .NET Maui" },
+                    { "MocaleTitle", "S3Mocale" },
+                }
+            };
+        }
+
+        if (cultureInfo.ToString() == "fr-FR")
+        {
+            return new ExternalLocalizationResult()
+            {
+                Success = true,
+                Localizations = new Dictionary<string, string>()
+                {
+                    { "CurrentLocaleName", "S3French" },
+                    { "LocalizationCurrentProviderIs", "S3Le fournisseur de localisation actuel est:" },
+                    { "LocalizationProviderName", "S3Json" },
+                    { "MocaleDescription", "S3Framework de localisation pour .NET Maui" },
+                    { "MocaleTitle", "S3Mocale" },
+                }
+            };
+        }
+
+        return new ExternalLocalizationResult()
+        {
+            Success = false,
+            Localizations = new Dictionary<string, string>(),
+        };
     }
 }
