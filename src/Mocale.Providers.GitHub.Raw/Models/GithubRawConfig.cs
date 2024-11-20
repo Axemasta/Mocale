342f6cca57a8499c4cb34cf405e8c04e62e83522
Axemasta <33064621+Axemasta@users.noreply.github.com>
2024-11-20 14:02:00 +0000

    Updated Github Provider
    Added documentation, updated dependencies, fixed warnings

--- src/Mocale.Providers.GitHub.Raw/Models/GithubRawConfig.cs
+++ src/Mocale.Providers.GitHub.Raw/Models/GithubRawConfig.cs
@@ -1,14 +1,20 @@
 namespace Mocale.Providers.GitHub.Raw.Models;
 
+/// <inheritdoc/>
 public class GithubRawConfig : IGithubRawConfig
 {
+    /// <inheritdoc/>
     public string Username { get; set; } = string.Empty;
 
+    /// <inheritdoc/>
     public string Repository { get; set; } = string.Empty;
 
+    /// <inheritdoc/>
     public string Branch { get; set; } = "main";
 
+    /// <inheritdoc/>
     public string LocaleDirectory { get; set; } = string.Empty;
 
+    /// <inheritdoc/>
     public IResourceFileDetails ResourceFileDetails { get; set; } = new JsonResourceFileDetails();
 }
